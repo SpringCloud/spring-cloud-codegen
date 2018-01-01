3804fad3d136c6e894b55ea8f616671c7c60acd8
gavin.xj <gavin.xj@alibaba-inc.com>
2018-01-01 17:38:13 +0800

    @RequestMapping("/api/v1")增加

--- spring-cloud-codegen-service/src/main/java/cn/springcloud/codegen/controller/SkeletonController.java
+++ spring-cloud-codegen-service/src/main/java/cn/springcloud/codegen/controller/SkeletonController.java
@@ -39,6 +39,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1")
 @Api(tags = { "脚手架接口" })
 public class SkeletonController {
     @Value("${skeleton.prefix.template.path}")
